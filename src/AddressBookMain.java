2f3f1d69cff9a5687fa4c28aa430f20a09acae61
surajseth90 <surajseth90@gmail.com>
2021-04-23 21:54:58 +0530

    Refactor the code successfully

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 public class AddressBookMain {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		AddContact addContact =new AddContact();
+	
+		System.out.println("Do you Want to Excecute : y/n ");
+		String next = scanner.nextLine();
 		
-		String next = sc.nextLine();
 		while(next.equalsIgnoreCase("y")) {
 		System.out.println("What you want to perform : ");
 		System.out.println("1. Add Contact ");
 		System.out.println("2. Edit Contact");
 		System.out.println("3. Remove Contact");
-		int input = sc.nextInt();
+		int input = scanner.nextInt();
+		
 		switch (input)
 		{
 		case 1:
@@ -22,22 +25,22 @@ public class AddressBookMain {
 		}
 		case 2:
 		{	
-			String name = sc.nextLine();
+			String name = scanner.nextLine();
 			ArrayList<AddressBook>contactDetails=addContact.getContact();
-			Search.searchContact(contactDetails, name);
+			UpdateContact.searchContact(contactDetails, name);
 			break;
 		}
 		case 3:
 		{
 			ArrayList<AddressBook>contactDetails=addContact.getContact();
 			System.out.println("Enter First name of contact to be removed");
-			String name =sc.nextLine();
+			String name =scanner.nextLine();
 			DeleteContact.deleteContact(contactDetails ,name);
 			break;
 		}
 		}
 		System.out.println("if you want to continue , press 'y'");
-		next = sc.nextLine();
+		next = scanner.nextLine();
 		}
 }
 }
--- src/DeleteContact.java
+++ src/DeleteContact.java
@@ -11,20 +11,6 @@ public class DeleteContact {
 		if(na.equals(name)){
 	      contactDetails.remove(contactDetails.get(i));
 		} 
-		
-	      System.out.println("Address Book after deletion");
-          for(int p=0;p<contactDetails.size();p++){
-        	    System.out.println("First name is : "+contactDetails.get(p).getFirstName());
-  				System.out.println("Last name is : " +contactDetails.get(p).getLastName());
-  				System.out.println("Address is : " +contactDetails.get(p).getAddress());
-  				System.out.println("City is : " +contactDetails.get(p).getCity());
-  				System.out.println("State is : " +contactDetails.get(p).getState());
-  				System.out.println("Zip is : " +contactDetails.get(p).getZip());
-  				System.out.println("Phone number is : " +contactDetails.get(p).getPhoneNumber());
-  				System.out.println("Email is : " +contactDetails.get(p).getEmail());
-           
-          }   
-         break; 
 		}
 	}
 }
